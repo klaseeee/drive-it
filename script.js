0c0fd7c8dbd3be0af43c8ebbe72c95dcb7bca44e
klaseeee <muklasardi9@gmail.com>
2021-05-17 16:10:43 +0700

    mengubah tampilan menu navbar pada mobile device

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 // menu toggle
 const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
 
 menuToggle.addEventListener('click', function() {
-    nav.classList.toggle('slide');
+    const burger = document.querySelector('nav ul');
+    const nav = document.querySelector('.nav');
+    burger.classList.toggle('slide');
+    nav.classList.toggle('hilang');
 });
 
+// scrolling
+const home = document.querySelector('nav ul li:first-child');
+home.addEventListener('click', function() {
+    window.location.href = '#gambar'
+});
 
+const cars = document.querySelector('nav ul li:nth-child(2)');
+cars.addEventListener('click', function() {
+    window.location.href = '#cars'
+});
+
+const term = document.querySelector('nav ul li:nth-child(3)');
+term.addEventListener('click', function() {
+    window.location.href = '#tc'
+});
+
+const about = document.querySelector('nav ul li:last-child');
+about.addEventListener('click', function() {
+    window.location.href = '#about'
+});
 
 
 // slideshow
@@ -25,8 +46,12 @@ menuToggle.addEventListener('click', function() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+        slideIndex = 1
+    }    
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";  
     }
